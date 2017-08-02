b74332f8d9a22a1acf307eeebb664fd0dfe170d1
Alexandre Beaurain <alexandre.beaurain@adfab.fr>
2017-08-02 15:21:42 +0200

    enhancement: ADLAB-37 deploy

--- bin/clean.sh
+++ bin/clean.sh
@@ -7,7 +7,7 @@ if [ -z "${DOMAIN}" ]
 then
 	DOMAIN=`basename ${DIR}`
 fi
-rm -Rf secrets secrets.tar.gz
+rm -Rf secrets secrets.tar.gz.enc bin/variables.sh bin/variables.sh.enc
 if [ ! -z "`echo ${DOMAIN} | grep '\.'`" ]
 then
 	curl -s -X BAN -H "Host: ${DOMAIN}" http://localhost/ -o /dev/null
