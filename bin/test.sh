9bb5c862678b3130a3c8cdd2e94938cc44b53b6b
Alexandre Beaurain <alexandre.beaurain@adfab.fr>
2017-08-02 14:44:38 +0200

    fix: ADLAB-37 test script

--- bin/test.sh
+++ bin/test.sh
@@ -5,5 +5,5 @@ cd ${LOCALDIR}/../web
 DIR=`pwd`
 php vendor/phpunit/phpunit/phpunit \
 	--configuration dev/tests/unit/phpunit.xml.dist \
-	--log-junit ${LOCALDIR}/../build/text.xml
+	--log-junit ${LOCALDIR}/../build/text.xml \
 	app/code/Adfab/Gdpr/Test/Unit/
