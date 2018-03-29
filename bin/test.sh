314e5538062dece0f03e3873fc4b113223c07c0f
Alexandre Beaurain <alexandre.beaurain@adfab.fr>
2018-03-29 16:41:12 +0200

    fix: test

--- bin/test.sh
+++ bin/test.sh
@@ -4,5 +4,5 @@ mkdir -p ${DIR}/build
 cd web
 php vendor/phpunit/phpunit/phpunit \
 	--configuration dev/tests/unit/phpunit.xml.dist \
-	--log-junit ${DIR}/../build/test.xml \
+	--log-junit ${DIR}/build/test.xml \
 	app/code/Adfab/Gdpr/Test/Unit/
