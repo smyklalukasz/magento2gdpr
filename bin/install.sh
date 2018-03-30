e147d78cc325922ba9c280e85b98f134dbe19667
Alexandre Beaurain <alexandre.beaurain@adfab.fr>
2018-03-30 11:46:38 +0200

    fix: build travis

--- bin/install.sh
+++ bin/install.sh
@@ -53,7 +53,7 @@ GRANT USAGE ON *.* TO '${DATABASE_USER}'@'localhost' IDENTIFIED BY '${DATABASE_P
 CREATE DATABASE IF NOT EXISTS \`${DATABASE_NAME}\` ;
 GRANT ALL PRIVILEGES ON \`${DATABASE_NAME}\`.* TO '${DATABASE_USER}'@'localhost' ;" | mysql -f
 	cd web
-	set +x
+	set +e
 	${PHP} bin/magento setup:install \
 		--db-host=localhost \
 		--db-name="${DATABASE_NAME}" \
@@ -71,7 +71,7 @@ GRANT ALL PRIVILEGES ON \`${DATABASE_NAME}\`.* TO '${DATABASE_USER}'@'localhost'
 		--admin-lastname=Adfab \
 		--cleanup-database \
 		--use-sample-data
-	set -x
+	set -e
 	cd ..
 fi
 if [ ! -z "${SHARED_DIR}" ]
